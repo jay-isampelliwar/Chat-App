dcc6a90b2144ac44a12fb1bc8e27cdfa7f10f662
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-07 17:59:53 +0530

    added model, appcolor and apptextStyle file

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color primary = Colors.black;
+  static Color secondary = Colors.white;
+}
---
+++ lib/core/constant/app_text_styles.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+
+class AppTextStyles {
+  static TextStyle text12({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.018,
+    );
+  }
+
+  static TextStyle text14({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.022,
+    );
+  }
+
+  static TextStyle text16({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.026,
+    );
+  }
+
+  static TextStyle text18({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.03,
+    );
+  }
+
+  static TextStyle text20({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.032,
+    );
+  }
+
+  static TextStyle text22({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.035,
+    );
+  }
+
+  static TextStyle text24({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.038,
+    );
+  }
+
+  static TextStyle text26({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.042,
+    );
+  }
+
+  static TextStyle text28({required bool bold, required Size size}) {
+    return TextStyle(
+      fontSize: ((size.width + size.height) / 2) * 0.044,
+    );
+  }
+}

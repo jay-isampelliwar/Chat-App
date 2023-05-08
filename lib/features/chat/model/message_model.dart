9fc807ad59f949966d6afd9baad548649b2a5f17
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-08 15:56:04 +0530

    added join and username props

--- lib/features/chat/model/message_model.dart
+++ lib/features/chat/model/message_model.dart
@@ -2,6 +2,13 @@ class Message {
   String message;
   DateTime date;
   bool isMe;
+  bool join;
+  String username;
 
-  Message({required this.message, required this.date, required this.isMe});
+  Message(
+      {required this.message,
+      required this.date,
+      required this.isMe,
+      this.join = false,
+      this.username = ""});
 }

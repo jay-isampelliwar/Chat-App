3b838aba1f756ab9118b3c6480691d3ea20a0127
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-10 23:52:02 +0530

    added Chat Bloc

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:socket_io_test/features/chat/bloc/chat_bloc.dart';
 import 'package:socket_io_test/features/lobby/bloc/lobby_bloc.dart';
 
 GetIt locator = GetIt.instance;
 
 void setup() {
   locator.registerLazySingleton<LobbyBloc>(() => LobbyBloc());
+  locator.registerLazySingleton<ChatBloc>(() => ChatBloc());
 }

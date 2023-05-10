38b3f172fe544373790beb4ff4b170cf71b585a2
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-10 23:52:38 +0530

    added initial event and bloc

--- lib/features/chat/bloc/chat_bloc.dart
+++ lib/features/chat/bloc/chat_bloc.dart
@@ -1,13 +1,30 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
+import "package:socket_io_client/socket_io_client.dart" as IO;
 part 'chat_event.dart';
 part 'chat_state.dart';
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
+  final IO.Socket _socket = IO.io("http://localhost/3000",
+      IO.OptionBuilder().setTransports(['websocket']).build());
   ChatBloc() : super(ChatInitial()) {
-    on<ChatEvent>((event, emit) {
-      // TODO: implement event handler
+    on<InitialChatSocketIOConnectionEvent>(initialChatSocketIOConnectionEvent);
+  }
+
+  FutureOr<void> initialChatSocketIOConnectionEvent(
+      InitialChatSocketIOConnectionEvent event, Emitter<ChatState> emit) {
+    _socket.onConnect((data) {
+      log("Connected io Socket");
+    });
+
+    _socket.onConnectError((data) {
+      log("Error in Socket");
+    });
+    _socket.onDisconnect((data) {
+      log("Disconnect to Socket");
     });
   }
 }
--- lib/features/chat/bloc/chat_event.dart
+++ lib/features/chat/bloc/chat_event.dart
@@ -6,3 +6,5 @@ abstract class ChatEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class InitialChatSocketIOConnectionEvent extends ChatEvent {}

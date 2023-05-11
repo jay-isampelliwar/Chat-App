d8a53db7687df09351018a6b81422a212a88424e
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-11 17:09:30 +0530

    changes in model

--- lib/features/chat/model/message_model.dart
+++ lib/features/chat/model/message_model.dart
@@ -1,14 +1,13 @@
 class Message {
   String message;
   DateTime date;
-  bool isMe;
   bool join;
   String username;
 
-  Message(
-      {required this.message,
-      required this.date,
-      required this.isMe,
-      this.join = false,
-      this.username = ""});
+  Message({
+    required this.message,
+    required this.date,
+    required this.username,
+    this.join = false,
+  });
 }

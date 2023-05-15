b0a297bf44c9cffc68e65c9318948a9e9be373ab
jay-isampelliwar <isampelliwar79@gmail.com>
2023-05-15 23:12:16 +0530

    Added New Colors

--- lib/core/constant/app_color.dart
+++ lib/core/constant/app_color.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color primary = const Color(0xFF212A3E);
-  static Color secondary = const Color(0xFF9BA4B5);
-  static Color backgroundColor = const Color(0xFF394867);
+  static Color backgroundColor = const Color(0xFFF1F6F9);
+  static Color joinCard = const Color(0xFF6DDABE);
+  static Color leaveCard = const Color(0xFFFC4F00);
+  static Color userMessageCardColor = const Color(0xFF394867);
+  static Color userMessageColor = Colors.white;
+  static Color friendMessageCardColor = const Color(0xFF212A3E);
+  static Color friendMessageColor = Colors.black;
 }
